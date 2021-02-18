a775466af4e5a500b0ceb7fa70d954c3a8b5f0f6
yinyin <yinyin.wang@helsinki.fi>
2021-02-18 19:17:16 +0200

    fifth_compass

--- ml_model/validation_ml.py
+++ ml_model/validation_ml.py
@@ -3,6 +3,11 @@ import pickle
 from scipy.stats import spearmanr, pearsonr
 from matplotlib import pyplot
 import matplotlib.pyplot as plt
+from lifelines import KaplanMeierFitter
+from lifelines.statistics import logrank_test
+import pickle
+import matplotlib.gridspec as gridspec
+
 
 def select_model(prediction_df, path):
 
@@ -28,9 +33,7 @@ def select_model(prediction_df, path):
     return best_model_dict
 
 
-
-
-def validate_commonpass(best_model_dict):
+def predict_commonpass(best_model_dict):
     pateint_group_dict = pickle.load(open('results/commpass/subgroup/pateint_group_log_dict', 'rb'))
 
     simple_name = {'Bortezomib': 'bor',
@@ -56,33 +59,65 @@ def validate_commonpass(best_model_dict):
 
     return pateint_group_dict_predicted
 
-def spearman_cor(pateint_group_dict):
 
-    level_dict = {'sCR': 6,
-                 'CR': 5,
-                 'VGPR': 4,
-                 'SD': 3,
-                 'PR': 2,
-                 'PD': 1
-                    }
+
+def evaluation_prediction(pateint_group_dict):
 
     # use correlation
-    for g, values in pateint_group_dict.items():
-        pateint_data = values['pateint_data']
+    def survival_time(pateint_data):
         pateint_data_selelct = pateint_data[(pateint_data['censpfs'] == 1) & pateint_data['censpfs'].notna()]
 
         expected = pateint_data_selelct['ttcpfs']
         predicted = pateint_data_selelct['predicted_response']
         cor, p_value = spearmanr(expected, predicted)
-        pateint_group_dict[g].update({'pfs_cor': cor,
-                                      'pfs_p_vlue': p_value})
         # plt.scatter(expected, predicted)
         # plt.show()
+        return cor, p_value
+
+    def groups_cox(pateint_data, ttc, cens, cut):
+        n = pateint_data.shape[0]//cut
+        pateint_data = pateint_data.sort_values(by='predicted_response')
+        T_h = pateint_data[ttc][-n:]
+        E_h = pateint_data[cens][-n:]
+        T_l = pateint_data[ttc][0:n]
+        E_l = pateint_data[cens][0:n]
+        results = logrank_test(T_h,
+                               T_l,
+                               event_observed_A=E_h,
+                               event_observed_B=E_l)
 
 
-    # use level correlation
-    for g, values in pateint_group_dict.items():
-        pateint_data = values['pateint_data']
+        def plot_figure(T_h,  E_h, T_l, E_l):
+            kmf_h = KaplanMeierFitter()
+            kmf_l = KaplanMeierFitter()
+
+            kmf_h.fit(durations=T_h,
+                      event_observed=E_h,
+                      label="higher expression (n={})".format(len(T_h)))
+            kmf_l.fit(durations=T_l,
+                      event_observed=E_l,
+                      label="lower expression (n={})".format(len(T_l)))
+
+            fig = plt.figure(figsize=(8, 8))
+            gs = gridspec.GridSpec(4, 1)
+            ax1 = fig.add_subplot(gs[0:2, :])
+            kmf_h.plot_survival_function(ax=ax1)
+            kmf_l.plot_survival_function(ax=ax1)
+            ax1.set_xlabel("Times of {}".format(ttc))
+            ax1.set_ylabel(" {} (%)".format(cens))
+            ax1.set_title("KMF of {} from cut off {}".format(g,  n))
+            plt.show()
+
+        return results
+
+    def repnose_level(pateint_data):
+        level_dict = {'sCR': 6,
+                      'CR': 5,
+                      'VGPR': 4,
+                      'SD': 3,
+                      'PR': 2,
+                      'PD': 1
+                      }
         pateint_data['reponse_level'] = pateint_data['bestrespsh'].apply(lambda x: level_dict.get(x))
         pateint_data_selelct_2 = pateint_data[pateint_data['reponse_level'].notna()]
 
@@ -90,24 +125,46 @@ def spearman_cor(pateint_group_dict):
         expected = pateint_data_selelct_2['reponse_level']
         predicted = pateint_data_selelct_2['predicted_response']
         cor, p_value = spearmanr(expected, predicted)
-        pateint_group_dict[g].update({'level_cor': cor,
-                                      'level_p_vlue': p_value})
         # plt.scatter(expected, predicted)
         # plt.show()
+        return cor, p_value
+
+    for g, values in pateint_group_dict.items():
+        pateint_data = values['pateint_data']
+
+        # by split top 10, bottom 10 group
+        ttc = 'ttcpfs'
+        cens= 'censpfs'
+        cut = 2
+        results = groups_cox(pateint_data, ttc, cens, cut)
+        pateint_group_dict[g].update({'group_test': results._test_statistic[0],
+                                      'group_p_vlue': results.p_value})
+
+        # by reponse level
+        cor, p_value = repnose_level(pateint_data)
+        pateint_group_dict[g].update({'level_cor': cor,
+                                      'level_p_vlue': p_value})
+
+        # by survival time of die patients
+        cor_s, p_value_s = survival_time(pateint_data)
+        pateint_group_dict[g].update({'pfs_cor': cor_s,
+                                      'pfs_p_vlue': p_value_s})
+
 
     return pateint_group_dict
 
 
 def main():
     path = 'results/module_ml_results/'
-    prediction_df = pd.read_csv(path + 'all_predition_result_top_fea.csv')
-    best_model_dict = select_model(prediction_df, path)
-    pickle.dump(best_model_dict, open(path+'best_model_dict', 'wb'))
-
-    pateint_group_dict = validate_commonpass(best_model_dict)
-    pickle.dump(pateint_group_dict, open(path + 'validate/commonpass/pateint_group_log_dict_prediected', 'wb'))
+    # prediction_df = pd.read_csv(path + 'all_predition_result_top_fea.csv')
+    # best_model_dict = select_model(prediction_df, path)
+    # pickle.dump(best_model_dict, open(path+'best_model_dict', 'wb'))
+    #
+    # pateint_group_dict = predict_commonpass(best_model_dict)
+    # pickle.dump(pateint_group_dict, open(path + 'validate/commonpass/pateint_group_log_dict_prediected', 'wb'))
 
     pateint_group_dict = pickle.load(open(path + 'validate/commonpass/pateint_group_log_dict_prediected', 'rb'))
 
-    pateint_group_dict_eval = spearman_cor(pateint_group_dict)
+    pateint_group_dict_eval = evaluation_prediction(pateint_group_dict)
     pickle.dump(pateint_group_dict_eval, open(path + 'validate/commonpass/pateint_group_dict_eval', 'wb'))
+
